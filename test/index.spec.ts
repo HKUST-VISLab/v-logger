7afcc2e70b4206f1166967ef077fb430c6ba4118
Ming Yao <yaoming.thu@gmail.com>
2017-05-11 18:57:26 +0800

    add more tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -6,9 +6,7 @@ import { BufferStream } from "./testUtils";
 test("test", async (t) => {
     // t.plan(1);
     const logger = new Logger("TEST", { logLevel: LogLevel.INFO});
-
     const stream = new BufferStream();
-
     logger.outStream(stream);
     logger.format("[:name] :level: :msg");
     logger.verbose("verbose");
@@ -23,21 +21,43 @@ test("test", async (t) => {
 });
 
 test("test date", async (t) => {
-    // t.plan(1);
-    const logger = new Logger("TEST", {logLevel: LogLevel.INFO});
-    logger.dateFormat("iso");
     const stream = new BufferStream();
-
-    logger.outStream(stream);
+    const logger = new Logger("TEST", {logLevel: LogLevel.INFO, stream});
+    logger.dateFormat("iso");
     logger.format("[:date] :name > :level: :msg");
     logger.log("WARN", () => {
         return new LogRecord(logger.name, LogLevel.WARN, "warn", new Date("10/21/2017 GMT"));
     });
-    logger.dateFormat("%a, %d %b %Y %H:%M:%S");
+    logger.dateFormat("utc");
     logger.log("ERROR", () => {
-        return new LogRecord(logger.name, LogLevel.ERROR, "error", new Date("11 May 2017 11:00:00"));
+        return new LogRecord(logger.name, LogLevel.ERROR, "error", new Date("10/21/2017 GMT"));
     });
-    /* tslint:disable no-console */
     return t.is(stream.buffer,
-                "[2017-10-21T00:00:00.000Z] TEST > WARN: warn\n[Thu, 11 May 2017 11:00:00] TEST > ERROR: error\n");
+                "[2017-10-21T00:00:00.000Z] TEST > WARN: warn\n[Sat, 21 Oct 2017 00:00:00 GMT] TEST > ERROR: error\n");
+});
+
+test("test date formatter", async (t) => {
+    const stream = new BufferStream();
+    const logger = new Logger("TEST", {logLevel: LogLevel.INFO, stream});
+    logger.dateFormat("%a %A %b %B %d %I %m %P %w %y %Y %H:%M:%S %% %Z |%c|%x|%X|%z");
+    logger.format("[:date]");
+    logger.log("ERROR", () => {
+        return new LogRecord(logger.name, LogLevel.ERROR, "error", new Date("Sat, 06 May 2017 19:40:00"));
+    });
+    const offset = new Date().getTimezoneOffset();
+    let z = offset < 0 ? "-" : "+";
+    z += ("0" + Math.floor(offset / 60)).slice(-2);
+    z += ("0" + offset % 60).slice(-2);
+    return t.is(stream.buffer,
+                "[Sat Saturday May May 06 07 05 PM 6 17 2017 19:40:00 %  " +
+                "|Sat May 06 2017|5/6/2017|7:40:00 PM|-0800]\n");
+});
+
+test("test date error format", async (t) => {
+    const stream = new BufferStream();
+    const logger = new Logger("TEST", {logLevel: LogLevel.INFO, stream});
+    const error = t.throws(() => {
+        logger.dateFormat("%u");
+    }, Error);
+    return t.is(error.message, "Currently do not support token \"u\"!");
 });
--- test/koa-logger.spec.ts
+++ test/koa-logger.spec.ts
@@ -6,7 +6,7 @@ import * as request from "supertest";
 import koaLogger from "./koa-logger";
 import { BufferStream } from "./testUtils";
 
-test("test koa logger", async (t) => {
+test("test koa logger normal", async (t) => {
     const app = new Koa();
     const stream = new BufferStream();
     app.use(koaLogger("Koa", {format: "common", stream}));
@@ -15,15 +15,25 @@ test("test koa logger", async (t) => {
         ctx.body = "Hello World";
         ctx.res.statusCode = 200;
     });
-    const req = request(app.listen(3000));
+    const req = request(app.listen());
     await req.get("/").expect(200);
-    // app.listen(3000);
-    // nock("localhost:3000")
-    //     .get("/")
-    //     .reply("Hello World");
-    /* tslint:disable no-console */
-    // console.log(res);
     t.regex(stream.buffer, new RegExp(
             ["\\:\\:ffff\\:127\\.0\\.0\\.1 \\[[\\d]{4}-[\\d]{2}-[\\d]{2}T[\\d]{2}",
             '\\:[\\d]{2}\\:[\\d]{2}\\.[\\d]{3}Z\\] \\"GET \\/ HTTP\\/1\\.1\\" 200 11 - [\\d]* ms'].join("")));
+});
+
+test("test koa logger error", async (t) => {
+    const app = new Koa();
+    const stream = new BufferStream();
+    app.use(koaLogger("Koa", {format: ":method :url :status :response[length]", stream}));
+    app.use(async (ctx, next) => {
+        await next();
+        ctx.body = "Hello World";
+        ctx.res.statusCode = 400;
+    });
+    const req = request(app.listen());
+    const res = await req.get("/test").expect(400);
+    /* tslint:disable no-console */
+    console.log(res);
+    t.regex(stream.buffer, /GET \/test 400 11/);
 });
